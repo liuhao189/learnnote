5cb0fcbc46fa400699062389602cc8fbc01c20da
liuhao21 <liuhao21@meituan.com>
2019-06-19 23:45:52 +0800

    编程工具

--- learn/go/code/goroutine.go
+++ learn/go/code/goroutine.go
@@ -4,19 +4,37 @@ import (
 	"fmt"
 )
 
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
 	}
-	c <- sum
+	close(c)
 }
 
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
-	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c
-	fmt.Println(x, y, x+y)
+	ch := make(chan int, 10)
+	go fibonacci(cap(ch), ch)
+	fmt.Println("invoked!")
+	for i := range ch {
+		fmt.Println(i)
+	}
 }
+
+// func sum(s []int, c chan int) {
+// 	sum := 0
+// 	for _, v := range s {
+// 		sum += v
+// 	}
+// 	c <- sum
+// }
+
+// func main() {
+// 	s := []int{7, 2, 8, -9, 4, 0}
+// 	c := make(chan int)
+// 	go sum(s[:len(s)/2], c)
+// 	go sum(s[len(s)/2:], c)
+// 	x, y := <-c, <-c
+// 	fmt.Println(x, y, x+y)
+// }
