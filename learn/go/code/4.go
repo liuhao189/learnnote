1069a50d4f35d8bee49c5f72ca1b3e4239a715b8
liuhao21 <liuhao21@meituan.com>
2019-06-23 00:14:34 +0800

    自说明代码

--- learn/go/code/4.go
+++ learn/go/code/4.go
@@ -4,42 +4,42 @@ import "fmt"
 import "unsafe"
 
 const (
-	Unknown=0;
-	Female=1;
-	Male=2;
+	Unknown = 0
+	Female  = 1
+	Male    = 2
 )
 
 const (
-	e="abc";
-	f=len(e);
-	g=unsafe.Sizeof(e);
+	e = "abc"
+	f = len(e)
+	g = unsafe.Sizeof(e)
 )
 
-func main(){
-	const LENGTH int=10;
-	const WIDTH int=5;
-	var area int;
-	const a,b,c=1,false,"str";
-	area= LENGTH * WIDTH;
-	fmt.Printf("Area is %d\n",area);
-	fmt.Println(a,b,c);
-	fmt.Println(Unknown,Female,Male);
+func main() {
+	const LENGTH int = 10
+	const WIDTH int = 5
+	var area int
+	const a, b, c = 1, false, "str"
+	area = LENGTH * WIDTH
+	fmt.Printf("Area is %d\n", area)
+	fmt.Println(a, b, c)
+	fmt.Println(Unknown, Female, Male)
 
-	fmt.Println(e,f,g);
+	fmt.Println(e, f, g)
 }
 
-func main(){
+func main() {
 	const (
-		a=iota;
-		b;
-		c;
-		d="ha";
-		e;
-		f=100;
-		g;
-		h=iota;
+		a = iota
+		b
+		c
+		d = "ha"
+		e
+		f = 100
+		g
+		h = iota
 		i
 	)
 
-	fmt.Println(a,b,c,d,e,f,g,h,i)
+	fmt.Println(a, b, c, d, e, f, g, h, i)
 }
